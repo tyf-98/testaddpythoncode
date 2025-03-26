931752ff5fcaf40439cae3d08fa4d7cd98d4a5e5
tyf-98 <791686874@qq.com>
2025-03-26 23:28:44 +0800

    testadd

--- pythonProject/test_0324.py
+++ pythonProject/test_0324.py
@@ -16,12 +16,12 @@ def test_a():  # test开头的测试函数
 
 def test_b():
     print("------->test_b")
-    assert 0  # 断言失败
+    assert 1  # 断言失败
 
 def test_c():
     print("------->test_b")
-    assert 0  # 断言失败
+    assert 1  # 断言失败
 
 
 if __name__ == '__main__':
-    pytest.main("-s  test_0324.py")  # 调用pytest的main函数执行测试
+    pytest.main(['-s','test_0324.py'])  # 调用pytest的main函数执行测试
